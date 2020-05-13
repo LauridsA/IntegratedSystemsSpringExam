14ea3345f31e598d42751517de30563e9f658790
LauridsA <Lauridsandersen2013@gmail.com>
2020-05-13 20:28:50 +0200

    MVP pipeline

--- CICD/AutomaticDeployment.sh
+++ CICD/AutomaticDeployment.sh
@@ -1,4 +1,8 @@
+cat ~/Dockerlogin/password.txt | docker login --username lauridsand --password-stdin
+docker build -f ../DependentService/Dockerfile -t lauridsand/entrypoint-service:latest ../DependentService/
+docker build -f ../EntrypointService/Dockerfile -t lauridsand/dependent-service:latest ../EntrypointService/
+#push it to the registry
+docker push lauridsand/entrypoint-service:latest
+docker push lauridsand/dependent-service:latest
 
-docker build -f ../DependentService/Dockerfile -t entrypoint-python:latest ../DependentService/
-docker build -f ../EntrypointService/Dockerfile -t dependent-service:latest ../EntrypointService/
-# kubectl apply -f deployment.yaml
+kubectl apply -f ../deployment.yaml
