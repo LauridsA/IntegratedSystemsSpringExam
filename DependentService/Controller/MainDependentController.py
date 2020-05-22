7cbb6d5e77d698f6a0407494a4176c46886f0cee
LauridsA <Lauridsandersen2013@gmail.com>
2020-05-22 21:02:23 +0200

    now with prime number calculations

--- DependentService/Controller/MainDependentController.py
+++ DependentService/Controller/MainDependentController.py
@@ -1,2 +1,9 @@
-def SomeFunction():
-    return 'Hello World New!'
+def CalculatePrimeNumber(self, fromValue, toValue):
+    res = []
+    for num in range(fromValue,toValue + 1):  
+        for i in range(2,num):  
+            if (num % i) == 0:  
+                break  
+        else:  
+            res.append(num) 
+    return res
--- DependentService/main.py
+++ DependentService/main.py
@@ -1,9 +1,13 @@
 from flask import Flask, request
 app = Flask(__name__)
+from Controller import MainDependentController as controller
 
 @app.route("/dependentservice/getPrimeNumbers",  methods=['POST'])
 def hello():
-    return request.form['to']
+    fromparam = request.form['from']
+    toparam = request.form['to']
+    res = controller.CalculatePrimeNumber(fromparam, toparam)
+    return res
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
