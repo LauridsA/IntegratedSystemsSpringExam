3ba5ca30c358452b8a3e6a3f6a9557d8cf768f95
LauridsA <Lauridsandersen2013@gmail.com>
2020-05-22 19:24:39 +0200

    fixed IP and docker file for dependent service

--- EntrypointService/main.py
+++ EntrypointService/main.py
@@ -11,4 +11,4 @@ def getPrimeNumber():
     return controller.GetPrimeNumber()
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8080) #dont forget to flip IP to 0.0.0.0
+    app.run(host='0.0.0.0', port=8080) #dont forget to flip IP to 0.0.0.0
